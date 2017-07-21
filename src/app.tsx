af53ca8500e3b3d5c7b21dc7ef6f19655e77a5df
taz0k <30292557+taz0k@users.noreply.github.com>
2017-07-22 01:41:57 +0200

    started implementing Demo

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,7 @@ import { Router, Route, IndexRoute, hashHistory } from "react-router";
 import Viewport from "./view/Viewport";
 import CollisionEditorRoute from "./view/CollisionEditorRoute";
 import MapEditorRoute from "./view/MapEditorRoute";
+import DemoRoute from "./view/DemoRoute";
 
 const app = document.getElementById('app');
 ReactDOM.render(
@@ -12,7 +13,7 @@ ReactDOM.render(
     <Route path="/" component={Viewport}>
       <IndexRoute component={MapEditorRoute}></IndexRoute>
       <Route path="collisions" component={CollisionEditorRoute}></Route>
-      <Route path="demo"></Route>
+      <Route path="demo" component={DemoRoute}></Route>
     </Route>
   </Router>,
 app);
