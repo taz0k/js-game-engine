84e3b67320cc1ea2848357d2407c773b3ccadd8d
taz0k <30292557+taz0k@users.noreply.github.com>
2017-08-10 15:36:47 +0200

    edit comments

--- src/view/pages/TodoRoute.tsx
+++ src/view/pages/TodoRoute.tsx
@@ -42,13 +42,6 @@ export default class TodoRoute extends React.Component {
 
 = TODO =
 
-* FpsCounter should listen to FRAME_WAS_DRAWN.
-* FRAME_WAS_DRAWN event should be triggered when frame has been drawn.
-
-* Remove from Demo "Show grid" "Load map" "Fill map with sprite".
-
-* Have an FPS-counter somewhere.
-
 * Remove my piece of crap "setInterval" and run the game at unlimited speed.
     Have all units be "pixels per second"
     and NOT, as now, pixels per frame!!
